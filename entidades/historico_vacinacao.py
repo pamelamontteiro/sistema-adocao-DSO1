de0ebcb46628dbf920fe90155b5cc4dc5ede1eba
Pamela Monteiro <pamela.csmonteiro@gmail.com>
2024-10-12 18:46:43 -0300

    chore

--- entidades/historico_vacinacao.py
+++ entidades/historico_vacinacao.py
@@ -1,23 +1,24 @@
-from datetime import date
 from vacina import Vacina
+from datetime import date
+
 
 class HistoricoVacinacao:
-    def __init__(self, data_vacinacao: date, vacina: Vacina)
-        self.__data_vacinacao = data
+    def __init__(self, data_de_vacinacao: date, vacina: Vacina):
+        self.__data_de_vacinacao = data_de_vacinacao
         self.__vacina = vacina
-    
+
     @property
-    def data_vacinacao(self):
-        return self.__data_vacinacao
-    
-    @data_vacinacao.setter
-    def data_vacinacao(self, date)
-        self.__data_vacinacao = data
-    
+    def data_de_vacinacao(self):
+        return self.__data_de_vacinacao
+
+    @data_de_vacinacao.setter
+    def data_de_vacinacao(self, data_de_vacinacao):
+        self.__data_de_vacinacao = data_de_vacinacao
+
     @property
-    def vacina(self)
+    def vacina(self):
         return self.__vacina
-    
+
     @vacina.setter
-    def vacina(self, Vacina)
+    def vacina(self, vacina):
         self.__vacina = vacina
