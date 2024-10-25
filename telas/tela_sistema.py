a3ad839f96aa1095a87d62937e1ef7bba23d5816
Pamela Monteiro <pamela.csmonteiro@gmail.com>
2024-10-25 16:02:59 -0300

    chore - atualização na tela de sistema, incluindo menu de adoção

--- telas/tela_sistema.py
+++ telas/tela_sistema.py
@@ -5,6 +5,7 @@ class TelaSistema:
         print("1 - Menu adotante")
         print("2 - Menu doadores")
         print("3 - Menu gato")
+        print("4 - Menu de adoção")
         print("0 - Finalizar sistema")
         opcao = int(input("Escolha a opcao: "))
         return opcao
